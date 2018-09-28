293a20a7e9f826fb10bebcad0dd24ea72657b5a6
root <>
2018-09-28 10:20:42 +0800

    完善sql

--- demo.sql
+++ demo.sql
@@ -6,20 +6,52 @@ create table demo_user (
   user_password varchar (32) not null ,
   user_email varchar (32) default '',
   nick_name varchar (16) default '',
-  user_img varchar (32) default '',
+  user_img varchar (64) default '',
   create_date datetime(3) default null ,
   update_date datetime(3) default null
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
+drop table if exists demo_role;
+create table demo_role(
+  role_id varchar(32) primary key,
+  role_code varchar (32) not null ,
+  role_name varchar (16) not null comment '角色名称',
+  start_date datetime not null comment '开始时间',
+  end_date datetime not null comment '结束日期',
+  valid tinyint(1) default 1 comment '是否有效，1有效，0无效'
+)ENGINE=InnoDB DEFAULT CHARSET=utf8 comment '角色信息表';
 drop table if exists demo_user_role;
 create table demo_user_role(
-  role_id varchar(32) not null primary key ,
+  user_role_id varchar(32) primary key,
+  role_id varchar(32) not null  ,
   user_id varchar(32) not null comment '用户id',
   role_name varchar (16) not null comment '角色名称',
-  role_coe varchar (16) not null comment '角色编码',
+  role_code varchar (16) not null comment '角色编码',
   start_date datetime not null comment '开始时间',
-  end_date datetime not null comment '结束日期'
-)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  end_date datetime not null comment '结束日期',
+  valid tinyint(1) default 1 comment '是否有效，1有效，0无效'
+)ENGINE=InnoDB DEFAULT CHARSET=utf8 comment '用户角色信息表';
+
+drop table  if exists demo_menu;
+create table demo_menu(
+  menu_id varchar (32) primary  key ,
+  menu_code varchar (64) not  null comment '菜单编码',
+  menu_name varchar (32) not null comment '菜单名称',
+  parent_menu_id varchar(32) not null comment '父菜单Id',
+  menu_addr varchar (64) not null  comment '菜单地址',
+  create_date datetime(3) not  null ,
+  create_code varchar(32) not null,
+  valid tinyint(1) default 1 comment '是否有效，1有效，0无效'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 comment '菜单信息表';
+
+drop table if exists demo_role_menu;
+create table demo_role_menu(
+  role_menu_id varchar (32) primary key,
+  role_id varchar (32) not null ,
+  role_code varchar (32) not null ,
+  menu_id varchar (32) not null ,
+  menu_addr varchar (64) not null ,
+  valid tinyint(1) default 1 comment '1 有效，0无效'
+)ENGINE=InnoDB DEFAULT CHARSET=utf8 comment '菜单角色对应表';
 
 drop table if exists demo_product;
 create table demo_product(
@@ -48,7 +80,7 @@ create table demo_product_items(
 	item_status tinyint(1) default 1 comment '是否有效；1有效；0无效'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-create table if exists demo_attachment_info 
+drop table if exists demo_attachment_info;
 create table demo_attachment_info(
 	attactment_id int(11) primary key auto_increment,
 	attactment_url varchar(64) default '' comment '地址',
@@ -59,11 +91,11 @@ create table demo_attachment_info(
 	create_date datetime default null,
 	create_by varchar(16) default '',
 	last_update_date datetime default null,
-	last_update_by varchar(16), 
+	last_update_by varchar(16),
 	attachment_status tinyint(1) default 1 comment '是否有效；1有效；0无效'
 )ENGINE=InnoDB comment '附件信息' DEFAULT CHARSET=utf8;
 
-create table if exists demo_code;
+drop table if exists demo_code;
 create table demo_code(
   id int(11) primary key auto_increment,
   code_type varchar (32) default null ,
@@ -78,15 +110,3 @@ create table demo_code(
 	last_update_by varchar(16)
 
 )ENGINE=InnoDB comment '编码' DEFAULT CHARSET=utf8;
-
-
-
-
-
-
-
-
-
-
-
-
